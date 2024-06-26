f4841c64133fe3dfd1f6357350e5112a293c1e35
veedanny <vinittasyvia3@gmail.com>
2024-06-26 23:38:24 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Vinitta Sylvia';
+const welcome = "Welcome to Flatbook";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
